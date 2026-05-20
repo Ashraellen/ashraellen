d43ee1d96c62e8e531f135056b0169484c1deef5
Ashraellen <ashraellen@gmail.com>
2026-05-20 08:45:24 +0200

    Add targeted Belarusian text fixes

--- assets/analytics.js
+++ assets/analytics.js
@@ -18,3 +18,25 @@
   script.src = 'https://www.googletagmanager.com/gtag/js?id=' + encodeURIComponent(GA_ID);
   document.head.appendChild(script);
 })();
+
+/* Ashraellen — tiny Belarusian text fixes */
+(function () {
+  'use strict';
+
+  var path = window.location.pathname;
+
+  if (path.indexOf('/be/public/posts/essay/') !== -1) {
+    var description = document.querySelector('meta[name="description"]');
+    if (description && description.content.indexOf('адзін пытанне') !== -1) {
+      description.content = description.content.replace('адзін пытанне', 'адно пытанне');
+    }
+  }
+
+  if (path.indexOf('/be/public/posts/essay/cycles/cycle-0001.html') !== -1) {
+    document.querySelectorAll('p').forEach(function (node) {
+      if (node.textContent.indexOf('у зместе цыклаў') !== -1) {
+        node.textContent = node.textContent.replace('у зместе цыклаў', 'у змесце цыклаў');
+      }
+    });
+  }
+})();
